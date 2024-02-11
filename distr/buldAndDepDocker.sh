e028e2881bda5ce81082ea9aede467a15f2b31f5
Ivan Kovalchuk <ISKovalchuk.SBT@sberbank.ru>
2024-02-11 18:18:36 +0700

    deploy coffee

--- distr/buldAndDepDocker.sh
+++ distr/buldAndDepDocker.sh
@@ -3,5 +3,5 @@ cd ..
 mv target/bot-1.0.jar distr/bot.jar
 docker build -t bot distr
 docker login
-docker tag bot bennyhils/bot:outline_ch_24.02.11
-docker push bennyhils/bot:outline_ch_24.02.11
+docker tag bot bennyhils/bot:outline_coffee_24.02.11
+docker push bennyhils/bot:outline_coffee_24.02.11

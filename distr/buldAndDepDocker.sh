74c15848a03969f2e7fcd7b6e180af9effec969a
Ivan Kovalchuk <ISKovalchuk.SBT@sberbank.ru>
2024-03-17 11:05:20 +0700

    2 prod deploy

--- distr/buldAndDepDocker.sh
+++ distr/buldAndDepDocker.sh
@@ -3,5 +3,5 @@ cd ..
 mv target/bot-1.0.jar distr/bot.jar
 docker build -t bot distr
 docker login
-docker tag bot bennyhils/bot:outline_dev_24.03.15
-docker push bennyhils/bot:outline_dev_24.03.15
+docker tag bot bennyhils/bot:ch_24.03.17
+docker push bennyhils/bot:ch_24.03.17
